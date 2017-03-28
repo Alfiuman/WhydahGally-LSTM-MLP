eee6d13408eeac39d0e50d200c6eed1e66d45d27
Alfiuman <alfiuman@yahoo.it>
2017-03-28 21:19:58 +0100

    new MathFunct

--- MathFunctions.h
+++ MathFunctions.h
@@ -11,10 +11,27 @@ namespace WhydahGally
 {
 	namespace Maths
 	{
-		float sigmoid(const float& x);
-		float derivativeSigmoid(const float& x);
+		template<typename T> T sigmoid(const T& x)
+		{
+			return 1 / (1 + exp(-x));
+		}
 
-		float sign(const float& x);
+		template<typename T> T derivativeSigmoid(const T& x)
+		{
+			return x * (1 - x);
+		}
+
+		template<typename T> T sign(const T& x)
+		{
+			if (x >= 0)
+			{
+				return 1.0f;
+			}
+			else
+			{
+				return -1.0f;
+			}
+		}
 
 		float mean(const std::vector<float>& x);
 		float mean(const std::vector<std::vector<float>>& x);
@@ -23,11 +40,57 @@ namespace WhydahGally
 		float sum(const std::vector<float>& x);
 		float sum(const Matrix& x);
 
-		float abs(const float& x);
+		template<typename T> T abs(const T& x)
+		{
+			if (x >= 0)
+			{
+				return x;
+			}
+			else
+			{
+				return -x;
+			}
+		}
+
 		std::vector<float> abs(const std::vector<float>& x);
 		std::vector<std::vector<float>> abs(const std::vector<std::vector<float>>& x);
 
-		float randNormalDistrib(const float& mean, const float& stdDev);
+		template<typename T> T randNormalDistrib(const T& mean, const T& stdDev)
+		{
+			static bool nBool = 0;
+			static float n = 0.0f;
+
+			if (!nBool)
+			{
+				float x;
+				float y;
+				float z;
+
+				do
+				{
+					x = 2.0f * rand() / RAND_MAX - 1.0f;
+					y = 2.0f * rand() / RAND_MAX - 1.0f;
+					z = pow(x, 2) + pow(y, 2);
+				} while (z > 1.0f || z == 0.0f);
+				{
+					float w = sqrt(-2.0f * log(z) / z);
+					float m = x * w;
+
+					n = y * w;
+
+					float result = m * stdDev + mean;
+					nBool = 1;
+
+					return result;
+				}
+			}
+			else
+			{
+				nBool = 0;
+
+				return n * stdDev + mean;
+			}
+		}
 	}
 }
 
